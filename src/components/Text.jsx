e276de6c48ec9b83e084b5d79f52941ae196b35c
Michael <mbg@outlook.com>
2018-10-02 16:08:17 +0100

    Update Text.jsx

--- src/components/Text.jsx
+++ src/components/Text.jsx
@@ -4,19 +4,6 @@ import PropTypes from 'prop-types'
 import { fontSize, fontWeight, color } from 'styled-system'
 import theme from '../theme'
 
-export const caps = props =>
-  props.caps
-    ? {
-        textTransform: 'uppercase'
-      }
-    : null
-
-export const regular = props =>
-  props.regular ? { fontWeight: props.theme.regular } : null
-
-export const bold = props =>
-  props.bold ? { fontWeight: props.theme.bold } : null
-
 export const italic = props => (props.italic ? { fontStyle: 'italic' } : null)
 
 const StyledText = styled.div`
@@ -37,21 +24,11 @@ const numberStringOrArray = PropTypes.oneOfType([
 
 Text.propTypes = {
   /** Font size */
-  fontSize: PropTypes.oneOfType([
-    PropTypes.number,
-    PropTypes.string,
-    PropTypes.array
-  ]),
+  fontSize: numberStringOrArray,
   /** Alignment */
   align: PropTypes.oneOf(['left', 'center', 'right', 'justify']),
-  /** Capitals */
-  caps: PropTypes.bool,
-  /** Regular */
-  regular: PropTypes.bool,
-  /** Bold */
-  bold: PropTypes.bool,
-  /** Italics */
-  italic: PropTypes.bool,
+  /** Font weight */
+  fontWeight: PropTypes.string,
   /** Color */
   color: PropTypes.string
 }
